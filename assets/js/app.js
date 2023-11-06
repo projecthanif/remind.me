0669740cfec2fcc8498b27d0a006a3648338f2e0
projecthanif <iamustapha213@gmail.com>
2023-11-06 23:30:51 +0100

    working on the model

--- assets/js/app.js
+++ assets/js/app.js
@@ -19,3 +19,23 @@ function navCollapes()
     let aside = document.getElementsByTagName("aside")[0];
     aside.style.display = "none";
 }
+
+
+let model = document.getElementById("model");
+
+model.addEventListener("click", modelToggle);
+
+function  modelToggle() 
+{
+    let model = document.getElementsByClassName("model")[0];
+    model.style.display = "block";
+}
+
+let modelClose = document.getElementById("model-close");
+modelClose.addEventListener("click", modelCollapse);
+
+function modelCollapse()
+{
+    let model = document.getElementsByClassName("model")[0];
+    model.style.display = "none";
+}
